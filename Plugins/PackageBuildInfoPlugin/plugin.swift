0c1a913939313c2843a901293b2cfc039d4b6f84
Samuel Défago <defagos@gmail.com>
2023-04-05 18:31:08 +0200

    Clean

--- Plugins/PackageBuildInfoPlugin/plugin.swift
+++ Plugins/PackageBuildInfoPlugin/plugin.swift
@@ -9,14 +9,12 @@ import Foundation
 @main
 struct PackageBuildInfoPlugin: BuildToolPlugin {
     func createBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
-        let repositoryPath = target.directory.appending("../..")
-        print("--> out dir: \(context.pluginWorkDirectory)")
-        return [
+        [
             .prebuildCommand(
                 displayName: "Get version",
                 executable: try context.tool(named: "PackageBuildInfo").path,
                 arguments: [
-                    "\(repositoryPath)",
+                    "\(target.directory.appending("../.."))",
                     "\(context.pluginWorkDirectory)"
                 ],
                 outputFilesDirectory: context.pluginWorkDirectory
